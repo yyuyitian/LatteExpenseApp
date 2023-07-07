4a1504652dca1afdaa62e0e134c58f8cfa86116b
yyuyitian <95139110+yyuyitian@users.noreply.github.com>
2023-07-08 07:30:07 +0800

    Initial Commit

---
+++ LatteExpense/LatteExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatteExpenseApp.swift
+//  LatteExpense
+//
+//  Created by 余伊甜 on 2023/7/8.
+//
+
+import SwiftUI
+
+@main
+struct LatteExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
